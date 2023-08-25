4555f71e278d73dc3293fbd2fb5584cec2d476eb
jjongs2 <jjongs@hanyang.ac.kr>
2023-08-25 07:32:43 +0000

    [feat] 동적 라우팅 구조 변경
    [...date] -> [date]

--- utils/parseDate.ts
+++ utils/parseDate.ts
@@ -1,8 +1,8 @@
 import type { ParsedUrlQuery } from 'querystring';
 
-export default function parseDate({ date: dateStrings }: ParsedUrlQuery): Date | null {
-  if (dateStrings === undefined) return null;
-  const date = new Date(dateStrings[0]);
+export default function parseDate({ date: dateString }: ParsedUrlQuery): Date | null {
+  if (typeof dateString !== 'string') return null;
+  const date = new Date(dateString);
   if (!(date instanceof Date)) return null;
   if (isNaN(date.getTime())) return null;
   return date;
