5f68f1542f3724651379115dd8195e3af73bf2f0
jjongs2 <jjongs@hanyang.ac.kr>
2023-08-25 11:07:02 +0000

    [feat] 페이지 이동 중 로딩 화면 표시

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import Loading from '@/components/Loading';
 import Layout from '@/components/layout/Layout';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import { Router, useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import type { ReactElement } from 'react';
 
@@ -10,30 +10,25 @@ import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
   const router = useRouter();
-
   const [hasToken, setHasToken] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // 쿠키 또는 세션 검사하고
-
-  // useEffect(() => {
-  //   const start = (): void => {
-  //     setLoading(true);
-  //   };
-  //   const end = (): void => {
-  //     setLoading(false);
-  //   };
-
-  //   Router.events.on('routeChangeStart', start);
-  //   Router.events.on('routeChangeComplete', end);
-  //   Router.events.on('routeChangeError', end);
-
-  //   return () => {
-  //     Router.events.off('routeChangeStart', start);
-  //     Router.events.off('routeChangeComplete', end);
-  //     Router.events.off('routeChangeError', end);
-  //   };
-  // }, []);
+  useEffect(() => {
+    const start = (): void => {
+      setIsLoading(true);
+    };
+    const end = (): void => {
+      setIsLoading(false);
+    };
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
 
   useEffect(() => {
     const accessToken = localStorage.getItem('accessToken');
@@ -51,6 +46,8 @@ export default function MyApp({ Component, pageProps }: AppProps): ReactElement 
     }
   }, [hasToken]);
 
+  if (isLoading) return <Loading />;
+
   return (
     <>
       <Head>
@@ -58,13 +55,9 @@ export default function MyApp({ Component, pageProps }: AppProps): ReactElement 
         <meta charSet='utf-8' />
         <meta name='description' content='모든 예약을 한 곳에' />
       </Head>
-      {loading ? (
-        <Loading />
-      ) : (
-        <Layout>
-          <Component pageProps={pageProps} />
-        </Layout>
-      )}
+      <Layout>
+        <Component pageProps={pageProps} />
+      </Layout>
     </>
   );
 }
