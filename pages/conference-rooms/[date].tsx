0ce279a95aeb15081f29142038baf48e295ed3d2
jjongs2 <jjongs@hanyang.ac.kr>
2023-08-28 14:04:18 +0000

    [feat] 회의실 예약 시 선택한 시간대 비트마스킹

--- pages/conference-rooms/[date].tsx
+++ pages/conference-rooms/[date].tsx
@@ -8,10 +8,33 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useRef, useState } from 'react';
 import type { ReactElement } from 'react';
 
+function getHoursIndex(time: Date): number {
+  const hours = time.getHours() + time.getMinutes() / 60;
+  return (hours - 8) * 2;
+}
+
 export default function Timetable(): ReactElement {
   const calendarRef = useRef(null);
   const router = useRouter();
   const [date, setDate] = useState<Date>();
+  const [startIndex, setStartIndex] = useState<number>();
+  const [endIndex, setEndIndex] = useState<number>();
+
+  function getTimeMask(): number {
+    let mask = 0b0;
+    for (let i = 0; i < 24; ++i) {
+      if (startIndex <= i && i < endIndex) {
+        mask |= 1 << i;
+      }
+    }
+    for (let i = 24; i < 30; ++i) {}
+    for (let i = 30; i < 32; ++i) {}
+    return mask;
+  }
+
+  function handleSubmitClick(): void {
+    console.log(getTimeMask().toString(2));
+  }
 
   useEffect(() => {
     if (date === undefined) return;
@@ -48,8 +71,9 @@ export default function Timetable(): ReactElement {
       resourceAreaWidth: '150px',
       resourceAreaColumns: [{ field: 'title' }],
       resourceGroupField: 'location',
-      select: function (info) {
-        console.log(info);
+      select: function ({ start, end }) {
+        setStartIndex(getHoursIndex(start));
+        setEndIndex(getHoursIndex(end));
       },
       selectable: true,
       selectOverlap: false,
@@ -92,7 +116,10 @@ export default function Timetable(): ReactElement {
 
   return (
     <div ref={calendarRef} className='mx-4 my-4'>
-      <button className='sticky mt-4 self-end rounded-[15px] bg-[#6A70FF] px-6 py-3 text-lg font-semibold text-white shadow-xl transition ease-in-out hover:bg-[#6AA6FF]'>
+      <button
+        onClick={handleSubmitClick}
+        className='sticky mt-4 self-end rounded-[15px] bg-[#6A70FF] px-6 py-3 text-lg font-semibold text-white shadow-xl transition ease-in-out hover:bg-[#6AA6FF]'
+      >
         Check-in
       </button>
     </div>
