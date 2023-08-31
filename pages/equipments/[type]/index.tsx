a4c5a3deeffbc56d52dc0f89228c0478d3835320
jjongs2 <jjongs@hanyang.ac.kr>
2023-08-31 13:24:46 +0000

    [style] 기자재 이미지 추가

--- pages/equipments/[type]/index.tsx
+++ pages/equipments/[type]/index.tsx
@@ -1,15 +1,22 @@
+import IMAGES from '@/assets/images';
 import Calendar from '@/components/calendar/Calendar';
-import ModalWrapper from '@/components/common/ModalWrapper';
 import type EquipmentsFormInfo from '@/interfaces/EquipmentsFormInfo';
 import apiController from '@/utils/apiController';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import type { ReactElement } from 'react';
 
+const DEVICE = {
+  0: { svg: IMAGES.laptop, device: 'MacBook' },
+  1: { svg: IMAGES.ipad, device: 'iPad' },
+  3: {},
+};
+
 export default function RentalList(): ReactElement {
   const router = useRouter();
   const [formInfos, setFormInfos] = useState<EquipmentsFormInfo[]>([]);
   const [type, setType] = useState<string | string[]>();
+  const [selectedDevice, setSelectedDevice] = useState<EquipmentsFormInfo>();
 
   useEffect(() => {
     setType(router.query.type);
@@ -27,22 +34,30 @@ export default function RentalList(): ReactElement {
     void fetchData();
   }, [type]);
 
+  const extensionDevice = (e) => {
+    setSelectedDevice(e);
+  };
+
   return (
     <>
-      {type === 'extension' && (
-        <ModalWrapper>
-          <div className='text-modal'>연장할 장비를 선택해 주세요.</div>
-          <div className='flex-col justify-center space-y-2'>
-            {formInfos.map(({ formId, equipment, returnDate }) => (
-              <div key={formId}>
-                <p>{equipment}</p>
-                <p>{returnDate}</p>
-              </div>
-            ))}
-          </div>
-        </ModalWrapper>
+      {type === 'extension' ? (
+        <div className='flex h-full items-center justify-evenly'>
+          {formInfos.map((item) => (
+            <div
+              key={item.formId}
+              className='flex flex-col items-center justify-center rounded-lg border-2 bg-slate-300 p-4 hover:bg-[#6AA6FF] hover:ring-2 hover:ring-[#6AA6FF] hover:ring-offset-1'
+              onClick={() => extensionDevice(item)}
+            >
+              <p className='h-44 w-44'>{DEVICE[item.equipment].svg}</p>
+              <p>{DEVICE[item.equipment].device}</p>
+              <p>반납 예정일 : {item.returnDate}</p>
+            </div>
+          ))}
+          {selectedDevice !== undefined ?? <div></div>}
+        </div>
+      ) : (
+        <Calendar />
       )}
-      <Calendar />
     </>
   );
 }
